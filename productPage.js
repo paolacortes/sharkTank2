3e05cd6ea58e4fa8e4174909cdb8d1f3e0a9e4fd
brianvelazquez <44505313+IsUDone@users.noreply.github.com>
2018-11-15 11:21:03 -0800

    updated HTML, CSS and JS files for the home page

---
+++ productPage.js
@@ -0,0 +1,23 @@
+$("#homeBTN").on("click", function(){
+document.querySelector("#homeContainer").scrollIntoView({
+  behavior: 'smooth'
+});
+});
+
+$("#aboutBTN").on("click", function(){
+document.querySelector("#aboutContainer").scrollIntoView({
+  behavior: 'smooth'
+});
+});
+
+$("#productBTN").on("click", function(){
+document.querySelector("#productContainer").scrollIntoView({
+  behavior: 'smooth'
+});
+});;
+
+$("#demoBTN").on("click", function(){
+document.querySelector("#demoContainer").scrollIntoView({
+  behavior: 'smooth'
+});
+});
